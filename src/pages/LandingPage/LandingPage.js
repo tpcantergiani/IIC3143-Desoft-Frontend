c4a4adef1b8eea3fb85107eb36edb713d760025e
mjaguirre <mjaguirre@uc.cl>
2021-04-16 18:53:56 -0400

    🐛 fix signin redirect

--- src/pages/LandingPage/LandingPage.js
+++ src/pages/LandingPage/LandingPage.js
@@ -1,9 +1,27 @@
 import React from 'react'
-import { Redirect } from 'react-router-dom'
+import { Link } from 'react-router-dom'
 
 const LandingPage = () => {
   return (
-    <Redirect to="/signin" />
+    <div
+      style={{
+        position: 'absolute',
+        left: 0,
+        top: 0,
+        bottom: 0,
+        right: 0,
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        flexDirection: 'column',
+      }}
+    >
+      <h2>Landing Page</h2>
+
+      <div>
+        <Link to="/home">Home</Link>
+      </div>
+    </div>
   )
 }
 export default LandingPage
